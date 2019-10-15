4f230e82912003d9a4b566815802f5518f200ff0
Logan Sandberg <Logan.Sandberg@healthsavings.com>
2019-10-15 16:25:12 -0400

    original commit

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class NewBehaviourScript
+{
+
+    public string myName;
+
+    public void AssingTileMaterial() {
+        GameObject[] tiles = GameObject.FindGameObjectsWithTag("Tiles");
+        Material material = Resources.Load<Material>("ping");
+
+        foreach(GameObject t in tiles) {
+            t.GetComponent<Renderer>().material = material;
+        }
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public string myname;
+    private int timer = 15;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer--;
+        if (timer < 0)
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + 1);
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - 1);
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.position = new Vector3(transform.position.x - 1, transform.position.y, transform.position.z);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.position = new Vector3(transform.position.x + 1, transform.position.y, transform.position.z);
+            }
+            timer = 15;
+        }
+
+    }
+}
